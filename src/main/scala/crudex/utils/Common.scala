aafb19779d9f8438675b1d5843615ae642ac9785
rpeszek <peszeks@gmail.com>
2017-07-15 15:31:34 -0600

    Refactored Http rendering helper methods to centralize response handling in one method. Experimented with use of type classes in Scala - actually quite nice stuff!

--- src/main/scala/crudex/utils/Common.scala
+++ src/main/scala/crudex/utils/Common.scala
@@ -21,21 +21,46 @@ import scalaz.effect.IO
 object Common {
   type Handler[A] = IO[A]
 
+  /*
+   Helper type defines common logic for Http response based on the rendered type.
+   Converts type A to displayable type B and decides the response code
 
-  //TODO nices type class solution is needed
-  def toJsonResponse[A](a: Handler[A])(implicit A: Encoder[A]): Task[Response] = {
-    Ok(a.unsafePerformIO.asJson)
-  }
-
-  def toJsonResponseWithOption[A](a: Handler[Option[A]])(implicit A: Encoder[A]): Task[Response] = {
-    val maybeA:Option[A] = a.unsafePerformIO
-    maybeA match {
-      case Some(ax)  => Ok(ax.asJson)
+   A is the type that comes from Handler
+   B is the type being rendered
+   ToHttpResponse can internally convert these types, e.g. Option[A] to A
+   */
+  private type ToHttpResponse[A,B] = A => EntityEncoder[B] => Task[Response]
+  private def optionToResponse[A]: ToHttpResponse[Option[A],A] = optA => encoderEv => {
+    //converts explicit  EntityEncoder[A] evidence to implicit evidence
+    implicit val ev: EntityEncoder[A] = encoderEv
+    optA match {
+      case Some(ax) => Ok(ax)
       case None => NotFound()
     }
   }
-
-  def toScalatagsHtml[A](a: Handler[A])(implicit A: EntityEncoder[A]): Task[Response] = {
-    Ok(a.unsafePerformIO)
+  private def defaultToResponse[A]: ToHttpResponse[A,A] = a => encoderEv => {
+    //converts explicit  EntityEncoder[A] evidence to implicit evidence
+    implicit val ev: EntityEncoder[A] = encoderEv
+    Ok(a)
   }
+
+  private def renderResponse[A,B](a: Handler[A], toHttpResponse: ToHttpResponse[A,B])(implicit encoderEv: EntityEncoder[B]): Task[Response]  =
+    toHttpResponse(a.unsafePerformIO)(encoderEv)
+
+  def toJsonResponse[A](a: Handler[A])(implicit A: Encoder[A]): Task[Response] = {
+    //brings evidence of EntityEncoder[A] based on Json (A: Encoder[A]) encoding evidence
+    implicit val ev: EntityEncoder[A] = jsonEncoderOf[A]
+    renderResponse(a, defaultToResponse[A])
+  }
+
+  def toJsonResponseWithOption[A](a: Handler[Option[A]])(implicit A: Encoder[A]): Task[Response] = {
+    //brings evidence of EntityEncoder[A] based on Json (A: Encoder[A]) encoding evidence
+    implicit val ev: EntityEncoder[A] = jsonEncoderOf[A]
+    renderResponse(a, optionToResponse[A])
+  }
+
+  def toScalatagsHtml[A](a: Handler[A])(implicit A: EntityEncoder[A]): Task[Response] =
+    renderResponse(a, defaultToResponse[A])
+
+
 }

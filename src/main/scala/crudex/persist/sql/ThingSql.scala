b480476d463c5135685d283f420fcd220ec0161c
rpeszek <peszeks@gmail.com>
2017-07-21 08:02:16 -0600

    Unit testing for doobie SQL - completed work

--- src/main/scala/crudex/persist/sql/ThingSql.scala
+++ src/main/scala/crudex/persist/sql/ThingSql.scala
@@ -13,14 +13,20 @@ import crudex.persist.sql.Common._
 import scala.reflect.runtime.universe.TypeTag
 
 /**
-  * Work in progress
   */
 object ThingSql {
+
+
+
+  /*
+     Query/Update methods are right next to actual method returning ConnectIO.
+     The split betten xyzSql methods returning Query or Update types allows
+     spec2 tests of sql correctness
+  */
 
   //TODO IList not typechecking directly
   //implicit def IListMeta[A: TypeTag](implicit ev: Meta[List[A]]): Meta[IList[A]] =
   //  ev.nxmap[IList[A]](IList.fromList, _.toList)
-
   def getThingsSql: Query0[ThingEntity] =
     sql"""
           SELECT id, name, description, user_id
@@ -30,30 +36,41 @@ object ThingSql {
      getThingsSql.list.map(IList.fromList)
 
 
-  def getThingSql (thingId: ThingId): Query0[Thing] =  sql"SELECT name, description, user_id FROM THING WHERE id=${thingId.id}".query[Thing]
+  def getThingSql (thingId: ThingId): Query0[Thing] =
+    sql"SELECT name, description, user_id FROM THING WHERE id=${thingId.id}".query[Thing]
   def getThing:  ThingId => ConnectionIO[Option[Thing]] = thingId =>
       getThingSql(thingId).option
 
-  def createThing :  Thing => ConnectionIO[ThingEntity] = thing =>
-    for {
-      id <- sql"SELECT ID_SEQ.nextval".query[Int].unique
-      _ <-  sql"INSERT INTO THING(id, name,description,user_id) VALUES (${id}, ${thing.name}, ${thing.description}, ${thing.userId})".update.run
-      //TODO fail if not inserted
-      //TODO re-retrieving thing would be better
-    } yield(Entity(ThingId(id), thing))
 
-  def modifyThing:  ThingId => Thing => ConnectionIO[Option[Thing]] = thingId => thing =>
+  def nextId : Query0[Int] = sql"SELECT ID_SEQ.nextval".query[Int]
+  def createThingSql: ThingId => Thing => Update0 = thingId => thing =>
+    sql"INSERT INTO THING(id, name,description,user_id) VALUES (${thingId.id}, ${thing.name}, ${thing.description}, ${thing.userId})".update
+  def createThing :  Thing => ConnectionIO[ThingEntity] = thing =>
      for {
-       i <- sql"""UPDATE THING
+        id <- nextId.unique
+        _ <-  createThingSql(ThingId(id))(thing).run
+        //TODO fail if not inserted
+        //TODO re-retrieving thing would be better
+     } yield(Entity(ThingId(id), thing))
+
+
+  def modifyThingSql: ThingId => Thing => Update0 = thingId => thing =>
+    sql"""UPDATE THING
             SET name = ${thing.name}, description = ${thing.description}, user_id = ${thing.userId}
             WHERE id = ${thingId.id}
-         """.update.run
+       """.update
+  def modifyThing:  ThingId => Thing => ConnectionIO[Option[Thing]] = thingId => thing =>
+     for {
+       i <- modifyThingSql(thingId)(thing).run
        //TODO re-retrieving thing would be better
      } yield(if(i===1) Some(thing) else None)
 
+
+  def deleteThingSql: ThingId =>  Update0 = thingId =>
+    sql"DELETE FROM THING WHERE id=${thingId.id}".update
   def deleteThing: ThingId =>  ConnectionIO[Unit] = thingId =>
     for {
-       _ <- sql"DELETE FROM THING WHERE id=${thingId.id}".update.run
+       _ <- deleteThingSql(thingId).run
        //TODO do something if nothing was deleted
     }yield(())
 
--- src/test/scala/sql/AnalysisTestSpec.scala
+++ src/test/scala/sql/AnalysisTestSpec.scala
@@ -6,13 +6,12 @@ package sql
 import crudex.persist.sql.ThingSql._
 import crudex.persist.sql.Common._
 import crudex.model._
+
 import doobie.h2.imports.H2Transactor
 import doobie.util.iolite.IOLite
 import doobie.specs2.imports._
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
-
-import scalaz.concurrent.Task
 
 
 trait dbSetup extends Scope {
@@ -20,12 +19,16 @@ trait dbSetup extends Scope {
 }
 object AnalysisTestSpec extends Specification with AnalysisSpec {
 
-  //val transactor = TestMe2.getTransactor
-    val transactor /*: Transactor[IOLite] */ = {
+  val transactor /*: Transactor[IOLite] */ = {
      H2Transactor[IOLite]("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "").unsafePerformIO
-    }
-  "this is the first example" in new dbSetup {
+  }
+
+  "check sql definitions for Thing entity" in new dbSetup {
      check(getThingsSql)
      check(getThingSql(ThingId(0)))
+     check(nextId)
+     check(createThingSql(ThingId(-1))(Thing("testNm","desc",None)))
+     check(modifyThingSql(ThingId(-1))(Thing("testNm","desc",None)))
+     check(deleteThingSql(ThingId(-1)))
   }
 }
